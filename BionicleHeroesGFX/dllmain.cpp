54c18d5e05100f167a59197e488aa39332eafd85
ermaccer <40604575+ermaccer@users.noreply.github.com>
2021-08-25 20:00:56 +0200

    add files

--- BionicleHeroesGFX/dllmain.cpp
+++ BionicleHeroesGFX/dllmain.cpp
@@ -1,19 +1,45 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include "MemoryMgr.h"
+#include "pattern/Hooking.Patterns.h"
+#include "IniReader.h"
+using namespace Memory::VP;
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+
+void Init()
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	CIniReader ini("");
+
+	bool	no_bloom = ini.ReadBoolean("Settings", "DisableBloom", false);
+	float	widescreen_val = ini.ReadFloat("Settings", "WidescreenValue", 9.0f / 16.0f);
+
+	// bloom
+	auto pattern = hook::pattern("52 6A 34 E8 ? ? ? ? 8B 44 24 38 50 E8 ? ? ? ?");
+
+	if (!pattern.count_hint(1).empty())
+	{
+		if (no_bloom)
+		{
+			auto ptr = pattern.get(0).get<int>(0);
+			Nop((int)ptr + 13, 5);
+		}
+
+	}
+
+	// widescreen value
+	pattern = hook::pattern("8B 46 08 C7 40 44 00 00 10 3F");
+
+	if (!pattern.count_hint(1).empty())
+	{
+		auto ptr = pattern.get(0).get<int>(0);
+		Patch<float>((int)ptr + 6, widescreen_val);
+	}
 }
 
+extern "C"
+{
+	__declspec(dllexport) void InitializeASI()
+	{
+		Init();
+	}
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# BionicleHeroesGFX
+BionicleHeroesGFX is a plugin for Bionicle Heroes which allows you to:
+
+- Disable bloom
+- Change scaling of Widescreen option
+
+### Bloom
+![](https://raw.githubusercontent.com/ermaccer/BionicleHeroesGFX/img/bloom.png)
+### Bloom Off
+![](https://raw.githubusercontent.com/ermaccer/BionicleHeroesGFX/img/bloomoff.png)
+
+You can configure these features in the .ini file.
+
+# Installation
+Download binary file from [Releases](https://github.com/ermaccer/BionicleHeroesGFX/releases) page then extract it to main game folder.
+
+# Building
+Built using Visual Studio 2017.
